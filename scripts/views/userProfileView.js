1c7310eb396e9fbf55905218634685093c14bc6f
evangelia-mitsopoulou <emitsopo@public-docking-pat-als-2107.ethz.ch>
2013-10-04 17:00:19 +0200

    Issue #8 Interaction box: user profile view

---
+++ scripts/views/userProfileView.js
@@ -0,0 +1,26 @@
+/**
+ * User Profile View
+ */
+function userProfileView(controller){
+	console.log("enter user profile view");
+	var self=this;
+	self.controller=controller;
+	self.tagID="userProfile";
+}
+
+userProfileView.prototype.open = function(){
+	console.log("enter open in user profile view");
+	this.update(); 
+};
+
+userProfileView.prototype.update= function(){
+	var self=this;
+
+	$("#welcome").empty();
+	$("#welcome").hide();
+
+	$("#userProfile").empty();
+	if ( self.controller.models.user.userProfile ) {
+		$("#userProfile").html("<p>Welcome to user profile view</p>");
+	}
+};
