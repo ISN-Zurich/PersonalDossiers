a4e6502353f3392b9eb76f56d20b63d128c6bdb6
evangelia-mitsopoulou <emitsopo@public-docking-pat-als-2107.ethz.ch>
2013-10-04 17:01:21 +0200

    interactionBox manages the transition between the various views of the items of the interaction box

---
+++ scripts/views/interactionBox.js
@@ -0,0 +1,27 @@
+/**
+ * this class describes general functionality of the interaction box
+ *  - handlers of click event on dossiers item/icon of the interaction box
+ *  - handlers of click event on user profile item/icon of the interaction box
+ * @param controller
+ */
+
+
+function interactionBox(controller){
+	console.log("enter interaction box constructor");
+	var self=this;
+	self.controller=controller;
+	
+	$("#st_dossiers").bind("click", function(){
+		console.log("clicked  dossiers icon");
+		self.controller.views.welcome.open();
+	});
+	
+	$("#st_user").bind("click", function(){
+		console.log("clicked  user profile icon");
+		self.controller.views.user.open();
+	});
+	
+	 $("#interaction_box").bind('click', function(){
+
+	 });
+}
