029851b9a0a5b34ff885dde1198d01f07e54f01a
evangelia-mitsopoulou <emitsopo@public-docking-pat-als-1770.ethz.ch>
2013-10-29 18:05:27 +0100

    single loop pattern Issue #23

--- scripts/views/welcomeView.js
+++ scripts/views/welcomeView.js
@@ -3,12 +3,27 @@
 function WelcomeView(controller){
 	var self=this;
 	self.controller=controller;
-	self.tagID="welcome"; 
+	self.tagID="welcomeBox"; 
 	$("#addDossierBtn").bind("click", function(e){
 		console.log("clicked the add Dossier button");
 		self.controller.models.dossierList.addDossier();
 	});
 
+	$("#welcomeBox").bind("click", function(e){
+		// ensure that clicks on the headers are ignored
+		
+		console.log("clicked the dossier category");
+		var targetE = e.target;
+		if (targetE.id !== "followingListHeader") {
+			var targetID = targetE.id;
+			var dosID = targetID.substring(7);
+			console.log("dosID is "+dosID);
+			var userModel = self.controller.models.user;
+			userModel.setActiveDossier(dosID);
+			window.location.href="index.html";
+			e.stopPropagation();
+		}
+	});
 }
 
 WelcomeView.prototype.openDiv=openView;
@@ -21,7 +36,7 @@ WelcomeView.prototype.open = function(){
 	$('#dossierListHeader').show();
 	console.log("called open of the WelcomeView");
 	this.update();
-	$("#welcome").show();
+	$("#" + this.tagID).show();
 	$('#'+this.tagID).removeClass("hidden");
 };
 
@@ -46,27 +61,34 @@ WelcomeView.prototype.update = function(){
 
 		console.log("dossier list is present design dossier list - welcome view");
 		dossierId=dossierListModel.getDossierId();
-
-
-		headerBlue =$("<div/>", {
-			"id": "dossierListHeader",
-			"class": "sidebar-header darkblue",
-			"text":"Personal Dossiers"
-		}).appendTo("#welcome");
-
-
-		// Design of the Dossier list that contain the Dossiers the user follows
-		header1 =$("<div/>", {
-			"id": "modifiedListHeader",
+		
+		welcomeElement=$("#welcome");
+		
+		//create non-optional elements
+		var l1 = this.createListBlock("L1");
+		welcomeElement.append(l1);
+		// create the optional elements
+		var hl2= $("<div/>", {
+			"id": "followingListHeader",
 			"class": "pd_grey_head",
-			"text":"Created / modified"
-		}).appendTo("#welcome");
+			"text":"Followed"
+		});
+		var l2 = this.createListBlock("L2");
+		
 
-
+		var isFollowedDossier, l2Uninitialized = false; 
 		do {
-			this.renderMyOwnItem();
+			isFollowedDossier = dossierListModel.isFollowedDossier();
+			if (isFollowedDossier && !l2Uninitialized ) {
+				l2Uninitialized = true;
+				welcomeElement.append(hl2);
+				welcomeElement.append(l2);
+			}
+			this.renderDossierItem(isFollowedDossier ? l2 : l1);
 		} while (dossierListModel.nextDossier());
 
+		return; // stop here
+		
 		// END OF DESIGN OF FIRST LIST OF DOSSIERS
 
 
@@ -85,206 +107,81 @@ WelcomeView.prototype.update = function(){
 		} while (dossierListModel.nextDossier());
 	}
 
-
-
-
-
-	$("#welcome").bind("click", function(e){
-		console.log("clicked the dossier category");
-		var targetE = e.target;
-		var targetID = targetE.id;
-		var dosID = targetID.substring(7);
-		console.log("dosID is "+dosID);
-		var userModel = self.controller.models.user;
-		userModel.setActiveDossier(dosID);
-		window.location.href="index.html";
-		e.stopPropagation();
-	});
-
-
-
-
 };
 
 WelcomeView.prototype.close=function(){
 	$('#'+this.tagID).addClass("hidden");
 };
 
-
-WelcomeView.prototype.renderMyOwnItem=function(div1){
-	console.log("enter redering my item");
-
-	var dossierListModel=self.controller.models.dossierList;
-//
-//	if (boolean){
-//		condition=!dossierListModel.isFollowedDossier(dossierListModel.index);
-//		console.log("condition 1is "+condition);
-//	}else{
-//		condition=dossierListModel.isFollowedDossier(dossierListModel.index);
-//		console.log("condition 2 is "+condition);
-//	}
-
-
-	if (!dossierListModel.isFollowedDossier()){
-
-console.log("enter if in condition");
-		div1 =$("<div/>", {
-			"class": "column featured2 hideOT dossier_item"
-		}).appendTo("#welcome");
-
-
-		div2 =$("<div/>", {
-			"class": "floatleft"
-		}).appendTo(div1);
-
-		img=$("<img/>", {
-			"class": "floatleft",
-			"src":self.controller.models.dossierList.getDossierImage(),
-			"width":"80px",
-			"height":"60px"
-		}).appendTo(div2);
-
-		div3 =$("<div/>", {
-			"class": "floatleft overviewcontent dossier_text"
-		}).appendTo(div1);
-
-		h1 =$("<h1/>", {
-			"class":"clickable",
-			"id" : "dossier" + self.controller.models.dossierList.getDossierId(),
-			"text":self.controller.models.dossierList.getDossierTitle()
-		}).appendTo(div3);
-
-		pS =$("<p/>", {
-			"class":"small",
-			// "text": "2013-07-08"
-		}).appendTo(div3);
-
-		p =$("<p/>", {
-			"class":"small",
-			// "text": "In this podcast, the former Foreign Minister of Indonesia, Hassan Wirajuda, outlines the thinking behind Jakarta&amp;rsquo;s current &amp;lsquo;free and active&amp;rsquo; foreign policy (bebas aktif); how the policy is shaping its relationships with the US, China and other ASEAN states; and how it might impact Indonesia&amp;rsquo;s 2014 presidential election."
-			"text":self.controller.models.dossierList.getDossierDescription()
-		}).appendTo(div3);
-		if (self.controller.oauth){
-			divDelContainer =$("<div/>", {
-				"class": "deletecontainer"
-			}).appendTo(div1);
-
-			divDel1 =$("<div/>", {
-				"id":"delete-",// dossierID the id of the dossier
-				"class": "deleteButton"
-			}).appendTo("#divDelContainer");
-
-			divDelConfirm =$("<div/>", {
-				"id":"delete-confirm",// dossierID the id of the dossier
-				"class": "deleteConfirmButton",
-				"text":"Really delete"
-			}).appendTo(divDelContainer);
-		}
-		lastbr1=$("<br/>", {
-
-		}).appendTo(div1);
-		lastbr2=$("<br/>", {
-
-		}).appendTo(div1);
-
-		if (dossierListModel.index < dossierListModel.dossierList.length-1){
-			hr=$("<hr/>", {
-				"class":"overview"
-			}).appendTo("#welcome");
-		}else{
-			hr=$("<hr/>", {
-				"class":"overview white"
-			}).appendTo("#welcome");
-		}
-	}
+WelcomeView.prototype.createListBlock = function(listId){
+	return $("<div/>", {
+		"id": listId
+	});
 };
 
-
-WelcomeView.prototype.renderFollowingItem=function(){
-	console.log("enter redering following item");
-
+WelcomeView.prototype.renderDossierItem = function(parentE) {
+	var self = this;
 	var dossierListModel=self.controller.models.dossierList;
-
-//	if (boolean){
-//		condition=!dossierListModel.isFollowedDossier(dossierListModel.index);
-//		console.log("condition 1is "+condition);
-//	}else{
-//		condition=dossierListModel.isFollowedDossier(dossierListModel.index);
-//		console.log("condition 2 is "+condition);
-//	}
+	
+	// This item should have a border by the CSS styles unless it is the last element in the parent container
+	div1 =$("<div/>", {
+		"class": "column featured2 hideOT dossier_item"
+	}).appendTo(parentE);
 
 
-	if (dossierListModel.isFollowedDossier(dossierListModel.index)){
+	div2 =$("<div/>", {
+		"class": "floatleft"
+	}).appendTo(div1);
 
-		console.log("enter if in condition");
-		div1 =$("<div/>", {
-			"class": "column featured2 hideOT dossier_item"
-		}).appendTo("#welcome");
+	img=$("<img/>", {
+		"class": "floatleft",
+		"src":self.controller.models.dossierList.getDossierImage(),
+		"width":"80px",
+		"height":"60px"
+	}).appendTo(div2);
 
+	div3 =$("<div/>", {
+		"class": "floatleft overviewcontent dossier_text"
+	}).appendTo(div1);
 
-		div2 =$("<div/>", {
-			"class": "floatleft"
+	h1 =$("<h1/>", {
+		"class":"clickable",
+		"id" : "dossier" + self.controller.models.dossierList.getDossierId(),
+		"text":self.controller.models.dossierList.getDossierTitle()
+	}).appendTo(div3);
+
+	pS =$("<p/>", {
+		"class":"small",
+		// "text": "2013-07-08"
+	}).appendTo(div3);
+
+	p =$("<p/>", {
+		"class":"small",
+		// "text": "In this podcast, the former Foreign Minister of Indonesia, Hassan Wirajuda, outlines the thinking behind Jakarta&amp;rsquo;s current &amp;lsquo;free and active&amp;rsquo; foreign policy (bebas aktif); how the policy is shaping its relationships with the US, China and other ASEAN states; and how it might impact Indonesia&amp;rsquo;s 2014 presidential election."
+		"text":self.controller.models.dossierList.getDossierDescription()
+	}).appendTo(div3);
+	if (self.controller.oauth){
+		divDelContainer =$("<div/>", {
+			"class": "deletecontainer"
 		}).appendTo(div1);
 
-		img=$("<img/>", {
-			"class": "floatleft",
-			"src":self.controller.models.dossierList.getDossierImage(),
-			"width":"80px",
-			"height":"60px"
-		}).appendTo(div2);
+		divDel1 =$("<div/>", {
+			"id":"delete-",// dossierID the id of the dossier
+			"class": "deleteButton"
+		}).appendTo("#divDelContainer");
 
-		div3 =$("<div/>", {
-			"class": "floatleft overviewcontent dossier_text"
-		}).appendTo(div1);
-
-		h1 =$("<h1/>", {
-			"class":"clickable",
-			"id" : "dossier" + self.controller.models.dossierList.getDossierId(),
-			"text":self.controller.models.dossierList.getDossierTitle()
-		}).appendTo(div3);
-
-		pS =$("<p/>", {
-			"class":"small",
-			// "text": "2013-07-08"
-		}).appendTo(div3);
-
-		p =$("<p/>", {
-			"class":"small",
-			// "text": "In this podcast, the former Foreign Minister of Indonesia, Hassan Wirajuda, outlines the thinking behind Jakarta&amp;rsquo;s current &amp;lsquo;free and active&amp;rsquo; foreign policy (bebas aktif); how the policy is shaping its relationships with the US, China and other ASEAN states; and how it might impact Indonesia&amp;rsquo;s 2014 presidential election."
-			"text":self.controller.models.dossierList.getDossierDescription()
-		}).appendTo(div3);
-		if (self.controller.oauth){
-			divDelContainer =$("<div/>", {
-				"class": "deletecontainer"
-			}).appendTo(div1);
-
-			divDel1 =$("<div/>", {
-				"id":"delete-",// dossierID the id of the dossier
-				"class": "deleteButton"
-			}).appendTo("#divDelContainer");
-
-			divDelConfirm =$("<div/>", {
-				"id":"delete-confirm",// dossierID the id of the dossier
-				"class": "deleteConfirmButton",
-				"text":"Really delete"
-			}).appendTo(divDelContainer);
-		}
-		lastbr1=$("<br/>", {
-
-		}).appendTo(div1);
-		lastbr2=$("<br/>", {
-
-		}).appendTo(div1);
-
-		if (dossierListModel.index < dossierListModel.dossierList.length-1){
-			hr=$("<hr/>", {
-				"class":"overview"
-			}).appendTo("#welcome");
-		}else{
-			hr=$("<hr/>", {
-				"class":"overview white"
-			}).appendTo("#welcome");
-		}
+		divDelConfirm =$("<div/>", {
+			"id":"delete-confirm",// dossierID the id of the dossier
+			"class": "deleteConfirmButton",
+			"text":"Really delete"
+		}).appendTo(divDelContainer);
 	}
-};
+	lastbr1=$("<br/>", {
+
+	}).appendTo(div1);
+	lastbr2=$("<br/>", {
+
+	}).appendTo(div1);
+
+}; 
 
