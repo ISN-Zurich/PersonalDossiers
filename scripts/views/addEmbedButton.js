9771600180a370d7b974a89345001e6fd6605279
evangelia-mitsopoulou <emitsopo@gess-sipo-als-dock-1-087.ethz.ch>
2013-11-14 18:11:37 +0100

    add the produced iframe code for the content embed page in the drop down page of embed View Issue #64

--- scripts/views/addEmbedButton.js
+++ scripts/views/addEmbedButton.js
@@ -9,6 +9,8 @@ function addEmbedButton(controller){
 		
 	$("#addEmbedBtn").bind("click", function(e){
 		console.log("clicked the add Embed button");
+		self.update();
+		$("#dropdown_embed").show();
 	});
 	
 }
@@ -17,12 +19,22 @@ addEmbedButton.prototype.openDiv=openView;
 
 addEmbedButton.prototype.open= function(){
 	console.log("open add embed View");
-	this.update();
+	//this.update();
 	this.openDiv();
 };
 
 addEmbedButton.prototype.update= function(){
+	
+	console.log("enter update in addEmbedd button");
 		
+	var self=this;
+	var inputContainer=$("<input/>", {
+		"type":"text",
+		"width":"200px",
+		"value":"<iframe src="+"http://yellowjacket.ethz.ch/tools/embedPageBig.html"+ "style="+ "width:475px;height:905px; border: none; overflow:hidden;></iframe>"
+	}).appendTo("#dropdown_embed");
+	
+	
 };
 
 addEmbedButton.prototype.closeDiv=closeView;
