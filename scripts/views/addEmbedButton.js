32f6f240edec9526cd0245f31b308341474ead2c
evangelia-mitsopoulou <emitsopo@gess-sipo-als-dock-1-087.ethz.ch>
2013-11-20 15:43:05 +0100

    drop down embedded  box fully functional and better layout. switch the code between  content and badge style embed Issue #64

--- scripts/views/addEmbedButton.js
+++ scripts/views/addEmbedButton.js
@@ -6,6 +6,7 @@ function addEmbedButton(controller){
 	var self=this;
 	self.controller=controller;
 	self.tagID="embedView";
+	self.dossierId=self.controller.getActiveDossier();
 		
 	$("#addEmbedBtn").bind("click", function(e){
 		console.log("clicked the add Embed button");
@@ -22,9 +23,21 @@ function addEmbedButton(controller){
 	
 	// TODO: add event handler when click on the content embed button
 	// - to show in the input container the link to the big embed html
+	$("#contentEmbed").bind("click", function(e){
+		$("#inputContainer").attr("value","<iframe scrolling=\"no\" src=\""+"http://yellowjacket.ethz.ch/tools/embedPageBig.html?id="+self.dossierId+"\""+" style= \""+ "width:475px;height:905px; border: none; overflow:hidden; \"></iframe>");
+		$("#badgeStyle").removeClass("pd_activeBadge");
+		$("#contentEmbed").addClass("pd_activeBadge");
+		
+	});
 	
 	//TODO:add event handler when click on the badge embed button
 	// - to show in the input container the link to the badge html
+	$("#badgeStyle").bind("click", function(e){
+		$("#inputContainer").attr("value","<iframe scrolling=\"no\" src=\""+"http://yellowjacket.ethz.ch/tools/embedBadge.html?id="+self.dossierId+"\""+" style= \""+ "width:250px;height:480px; border: none; overflow:hidden; \"></iframe>");
+		$("#contentEmbed").removeClass("pd_activeBadge");
+		$("#badgeStyle").addClass("pd_activeBadge");
+		
+	});
 	
 }
 
@@ -51,7 +64,7 @@ addEmbedButton.prototype.update= function(){
 	var dossierId=this.controller.getActiveDossier();
 	$("#addEmbedBtn").css("margin-bottom", "1px");
 	
-	$("#inputContainer").attr("value","<iframe src=\" "+"http://yellowjacket.ethz.ch/tools/embedPageBig.html?id="+dossierId+"\""+" style= \""+ "width:475px;height:905px; border: none; overflow:hidden; \"></iframe>");
+	$("#inputContainer").attr("value","<iframe scrolling=\"no\" src=\""+"http://yellowjacket.ethz.ch/tools/embedPageBig.html?id="+dossierId+"\""+" style= \""+ "width:475px;height:905px; border: none; overflow:hidden; \"></iframe>");
 	
 	$("#inputContainer").bind("click",function(){
 		$("#inputContainer").select();
