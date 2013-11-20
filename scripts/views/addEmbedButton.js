92953775a2ffc3bf0906434923dd17c751e077e6
evangelia-mitsopoulou <emitsopo@gess-sipo-als-dock-1-087.ethz.ch>
2013-11-20 18:11:55 +0100

    first draft on how to get the detailed embed view of pdfs Issue #68

--- scripts/views/addEmbedButton.js
+++ scripts/views/addEmbedButton.js
@@ -20,9 +20,15 @@ function addEmbedButton(controller){
 		}
 	});
 	
+	$("#inputContainer").bind("focus",function(){
+		$("#inputContainer").select();
+	});
+
+	$("#inputContainer").bind("click",function(){
+		$("#inputContainer").select();
+	});
 	
-	// TODO: add event handler when click on the content embed button
-	// - to show in the input container the link to the big embed html
+
 	$("#contentEmbed").bind("click", function(e){
 		$("#inputContainer").attr("value","<iframe scrolling=\"no\" src=\""+"http://yellowjacket.ethz.ch/tools/embedPageBig.html?id="+self.dossierId+"\""+" style= \""+ "width:475px;height:905px; border: none; overflow:hidden; \"></iframe>");
 		$("#badgeStyle").removeClass("pd_activeBadge");
@@ -30,8 +36,6 @@ function addEmbedButton(controller){
 		
 	});
 	
-	//TODO:add event handler when click on the badge embed button
-	// - to show in the input container the link to the badge html
 	$("#badgeStyle").bind("click", function(e){
 		$("#inputContainer").attr("value","<iframe scrolling=\"no\" src=\""+"http://yellowjacket.ethz.ch/tools/embedBadge.html?id="+self.dossierId+"\""+" style= \""+ "width:250px;height:480px; border: none; overflow:hidden; \"></iframe>");
 		$("#contentEmbed").removeClass("pd_activeBadge");
@@ -66,9 +70,9 @@ addEmbedButton.prototype.update= function(){
 	
 	$("#inputContainer").attr("value","<iframe scrolling=\"no\" src=\""+"http://yellowjacket.ethz.ch/tools/embedPageBig.html?id="+dossierId+"\""+" style= \""+ "width:475px;height:905px; border: none; overflow:hidden; \"></iframe>");
 	
-	$("#inputContainer").bind("click",function(){
-		$("#inputContainer").select();
-	});
+	$("#inputContainer").focus();
+	
+
 	
 	$("#drop_info").show();
 
--- service/streamtest.php
+++ service/streamtest.php
@@ -1,6 +1,10 @@
 <?php
 require_once('HTTP/Request.php');
 require_once('HTTP/Request/Listener.php');
+require_once 'MDB2.php';
+include 'dbConnect.php';
+
+
 
 // get the pdf id 
 
@@ -36,6 +40,47 @@ class HTTPStreamer extends HTTP_Request_Listener
 }
 
 // use the pdf id
+
+// get the id for the specific item from the URL query string $_GET["id"]
+	$item_id=$_GET["id"];
+	error_log("item id id".$item_id);
+// get the JSON data for the id from the database
+// 	$query='SELECT metadata FROM library_metadata WHERE id=?';
+// 	$statement = $mdb2->prepare($query);
+	
+// 	$res= $statement->execute($item_id);
+		
+	$mdb2->setFetchMode(MDB2_FETCHMODE_ASSOC);
+
+ 	$sth = $mdb2->prepare('SELECT metadata FROM library_metadata  WHERE digital_library_id=?');
+ 	 	
+ 	if (PEAR::isError($sth)) {
+ 			error_log("pear error " . $sth->getMessage());
+ 			exit;
+ 			}
+ 		
+	$res = $sth->execute($item_id);
+	
+// 	if (PEAR::isError($res)) {
+// 		$this->log("pear error " . $res->getMessage());
+// 		$this->bad_request();
+// 		$sth->free();
+// 		return;
+// 	}
+
+// 	$idata= array();
+	
+// 	while ($row=$res->fetchRow()) {
+// 		//parse the item meta data 
+// 		$row["metadata"] = json_decode($row["metadata"]);
+// 		array_push($idata,$row);
+// 	}
+	
+// 	error_log("metadata are ".$idata);
+	
+// check if this is indeed a pdf item, and them get its identifier and pass it as an argument to the Http_Request
+
+	
 $r = new HTTP_Request("http://mercury.ethz.ch/serviceengine/Files/ISN/136414/ipublicationdocument_singledocument/cdb593a6-7909-4cf1-b812-03bbe49405d7/en/IB_ChinasWhitePapersonSpaceAnAnalysis.pdf", array('method'=> "GET"));
 
 $r->attach(new HTTPStreamer());
