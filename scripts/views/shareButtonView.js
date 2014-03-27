5fa25d616f1017f68d551b293e0e0eadd2e558c1
Marcus Klinge <emp@captainklinge.de>
2014-03-27 13:31:34 +0100

    Changed facebook sharer.php code
    Seems the p[andthensome] syntax has changed

--- scripts/views/shareButtonView.js
+++ scripts/views/shareButtonView.js
@@ -25,12 +25,12 @@ function ShareButtonView(controller){
 		
 		var whole_image_string=window.location.href;
 		var trim_url=whole_image_string.substring(0, whole_image_string.indexOf('index.html'));
-		ISNLogger.log("trimed url is "+trim_url);
+		ISNLogger.log("trimmed url is "+trim_url);
 		
 		var shared_image= encodeURIComponent(trim_url+'/'+bookmarkModel.getDossierImageURL());
 		ISNLogger.log("sharedimage is "+shared_image);
 
-		window.open('http://www.facebook.com/sharer.php?s=100&p[title]='+ shared_title + '&p[summary]=' + shared_description + '&p[u]=' + shared_url_fb + '&p[images][0]='+shared_image, 
+		window.open('http://www.facebook.com/sharer.php?s=100&[title]='+ shared_title + '&[summary]=' + shared_description + '&[u]=' + shared_url_fb + '&[images][0]='+shared_image, 
 				 'facebook-share-dialog', 
 				 'width=626,height=436'		
 		);
