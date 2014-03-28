d23c3ea242d42d18970d4259add4a14cd6b95339
Marcus Klinge <emp@captainklinge.de>
2014-03-28 11:24:54 +0100

    minor

--- share.php
+++ share.php
@@ -1,6 +1,5 @@
-<!-- 
-This page will be shared with social media platforms that don't understand how to render the javascript (facebook for start)
--->
+// This page will be shared with social media platforms that don't understand how to render the javascript (facebook for start)
+
 
 <!DOCTYPE html>
 <html lang="en">
