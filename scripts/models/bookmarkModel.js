9498a13a3c29bdfaf39d8efe27180d668bf61fb9
Mark Yasuda <yasuda@sipo.gess.ethz.ch>
2014-04-03 12:32:59 +0200

    replace console.log references with ISNLogger

--- scripts/models/bookmarkModel.js
+++ scripts/models/bookmarkModel.js
@@ -561,7 +561,7 @@ BookmarkModel.prototype.getEmbedURL = function() {
     if ( itemType !== 'Audio') {
         itemType = itemType + 's';
     }
-    
+
     return 'http:\/\/www.isn.ethz.ch\/layout\/set\/pdembed\/Digital-Library\/'+ itemType +'\/Detail\/?lng=en&id='+this.getItemId();
 };
 
@@ -575,16 +575,16 @@ BookmarkModel.prototype.getUsertype = function() {
 
 /**
  * @method checkUserRole(user, role)
- * 
+ *
  * This method checks if a given user id has a role in the current dossier
  */
 BookmarkModel.prototype.checkUserRole = function(userid, role) {
     var i;
-    console.log('check user: ' + userid);
+    ISNLogger.log('check user: ' + userid);
     for (i = 0; i < this.userlist.length; i++) {
-         console.log('user id is ' + this.userlist[i].user_id);
+         ISNLogger.log('user id is ' + this.userlist[i].user_id);
         if (userid && this.userlist[i].user_id === userid) {
-             console.log( 'role is "' + this.userlist[i].user_type + '" compare with "' +role +'"' );
+             ISNLogger.log( 'role is "' + this.userlist[i].user_type + '" compare with "' +role +'"' );
             if (role && this.userlist[i].user_type === role) {
                 return true;
             }
